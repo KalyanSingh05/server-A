5e6d3783bdc17f9d67d12812222b4de8f6ae6336
Kalyan Singh <95122829+KalyanSingh05@users.noreply.github.com>
2024-07-13 19:28:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send(`
         <h1>Server A</h1>
-        <p>To access the secret file, you need to send a GET request to <button><a href ="http://localhost:4000/file">Go To LAB</a></button> with the correct authorization token.</p>
+        <p>To access the secret file, you need to send a GET request to <button><a href ="https://cors-2-2qo7.onrender.com/file">Go To LAB</a></button> with the correct authorization token.</p>
         <p>Use the appropriate tools to bypass CORS and fetch the content of the file.</p>
     `);
 });
